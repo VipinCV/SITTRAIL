5c26aad4367532e1fc46d23aba8dd1bf05bd9bab
Vipin <vipincvmavila@gmail.com>
2026-03-20 21:57:24 +0530

    Add Location model for GPS tracking

---
+++ Models/Location.cs
@@ -0,0 +1,12 @@
+namespace gps_tracking_api.Models
+{
+    public class Location
+    {
+        public int Id { get; set; }
+        public Guid UserId { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public double Speed { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
