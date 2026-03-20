13c9d95538cde3d2ebbd7d7446e4a6d7c2409a5b
Vipin <vipincvmavila@gmail.com>
2026-03-20 21:51:00 +0530

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+using gps_tracking_api.Data;
+using gps_tracking_api.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Render PORT fix
+builder.WebHost.ConfigureKestrel(options =>
+{
+    var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+    options.ListenAnyIP(int.Parse(port));
+});
+
+// Services
+builder.Services.AddControllers();
+builder.Services.AddSignalR();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy => policy.AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowAnyOrigin());
+});
+
+var app = builder.Build();
+
+app.UseCors("AllowAll");
+
+app.MapControllers();
+app.MapHub<TrackingHub>("/trackingHub");
+
+app.Run();
