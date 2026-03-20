96dfa2737fbe63a39a214d1512148164f88b351b
Vipin <vipincvmavila@gmail.com>
2026-03-20 22:01:45 +0530

    Add TrackingController for location updates

---
+++ Controllers/TrackingController.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+using gps_tracking_api.DTOs;
+using gps_tracking_api.Models;
+using gps_tracking_api.Data;
+using Microsoft.AspNetCore.SignalR;
+using gps_tracking_api.Hubs;
+
+namespace gps_tracking_api.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class TrackingController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        private readonly IHubContext<TrackingHub> _hub;
+
+        public TrackingController(AppDbContext context, IHubContext<TrackingHub> hub)
+        {
+            _context = context;
+            _hub = hub;
+        }
+
+        [HttpPost("location")]
+        public async Task<IActionResult> Save(LocationDto dto)
+        {
+            var loc = new Location
+            {
+                UserId = dto.UserId,
+                Latitude = dto.Lat,
+                Longitude = dto.Lng,
+                Speed = dto.Speed,
+                CreatedAt = DateTime.UtcNow
+            };
+
+            _context.Locations.Add(loc);
+            await _context.SaveChangesAsync();
+
+            await _hub.Clients.All.SendAsync("ReceiveLocation", loc);
+
+            return Ok(loc);
+        }
+    }
+}
