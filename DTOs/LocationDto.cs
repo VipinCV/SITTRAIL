56dfc23d903f6aa1d546ea7f814dbd2de6d997ba
Vipin <vipincvmavila@gmail.com>
2026-03-20 21:59:06 +0530

    Add LocationDto class for GPS tracking

---
+++ DTOs/LocationDto.cs
@@ -0,0 +1,10 @@
+namespace gps_tracking_api.DTOs
+{
+    public class LocationDto
+    {
+        public Guid UserId { get; set; }
+        public double Lat { get; set; }
+        public double Lng { get; set; }
+        public double Speed { get; set; }
+    }
+}
